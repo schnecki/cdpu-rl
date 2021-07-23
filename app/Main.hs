00c5baa4b08075500330773d11babc1b4d4dcb4b
Manuel Schneckenreither <manuel.schneckenreither@uibk.ac.at>
2021-07-23 14:22:12 +0200

    instance generator

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import qualified Data.Text             as T
+import           System.Directory
+import           System.Environment
+import           System.FilePath.Posix
 
 import           Instance.Generator
 import           Instance.Writer
 
 main :: IO ()
-main = generateInstance True 0.30 100 >>= writeInstance "testInstance"
+main = do
+  args <- getArgs
+  let size
+        | null args = 10
+        | otherwise = read (head args)
+  let directory
+        | length args <= 1 = "../code/instances"
+        | otherwise = args !! 1
+  inst <- generateInstance True 0.30 size
+  dLs <- map T.pack <$> listDirectory directory
+  let dLsFiltered = filter (T.isPrefixOf (T.pack (show size) <> "_")) dLs
+      nr = 1 + length dLsFiltered
+      instanceName = show size ++ "_" ++ show nr ++ ".txt"
+
+  writeInstance (directory </> instanceName) inst
